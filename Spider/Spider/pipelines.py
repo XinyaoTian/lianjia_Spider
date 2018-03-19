6fb64ceaffa96e0c8be891ac18cf29e089ae1a08
XinyaoTian <31195026+XinyaoTian@users.noreply.github.com>
2018-03-19 14:34:25 +0800

    Now the program can read json by using get_comhrefs() in func_pack.py

--- Spider/Spider/pipelines.py
+++ Spider/Spider/pipelines.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# 注意json文件以一个空的字典{}结尾！
 import json
 import codecs
 # Define your item pipelines here
@@ -9,13 +10,24 @@ import codecs
 class JsonWithEncodingPipeline(object):
     def __init__(self):
         self.file = codecs.open('href.json', 'w', encoding='utf-8')
-        #self.file.write('[')
+        self.file.write('[')
+        #print "Open the spider pipeline"
+
     def process_item(self, item, spider):
         line = json.dumps(dict(item), ensure_ascii=False) + ",\n"
         self.file.write(line)
         return item
-    def spider_closed(self, spider):
-        #self.file.write(']')
+
+    #要特别注意这两个函数名，因为框架需要调用 因此名字必须是open_spider 一点都不能错
+    def open_spider(self,spider):
+        pass
+        #print "Spider start!"
+
+    #同样 close_spider 的函数名也必须是这个，否则框架是无法识别的
+    def close_spider(self, spider):
+        #print "Close the spider pipeline"
+        #注意Json文件以一个空的数据结构结尾
+        self.file.write('{}]')
         self.file.close()
 
 class SpiderPipeline(object):
---
+++ Spider/func_pack.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+import json
+
+#用于获取已经取得的全部社区的url地址
+def get_comhrefs():
+    with open("href.json",'r')as f:
+        temp = json.loads(f.read())
+        #注意json文件以一个空的字典{}结尾！
+    f.close()
+    list_href = []
+    for item in temp:
+        if bool(item) is True: #由于json文件中最后一个数据为空，因此设置此判断防止误读
+            list_href.append(item["href_community"])
+    return list_href
+
+
+#Testing code
+#print get_comhrefs()
+#print(temp)
+#print(temp[1]['href_community'])
